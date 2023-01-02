72f633daf66275d52f74d945dc628a27a8ef435f
junsangyooo <dbwnstkddb@gmail.com>
2023-01-01 21:57:31 -0500

    update
    update

---
+++ LeetCode_75/Find_Pivot_Index.py
@@ -0,0 +1,23 @@
+# 724. Find Pivot Index
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = 0
+        for num in nums:
+            # if index == 0:
+            #     continue    
+            right += num
+            
+        for index, num in enumerate(nums):
+            right -= num
+            if left == right:
+                return index
+            else:
+                left += num
+
+        return -1
+        
