5fbe8fda44443f6a05d245816f0f98dd007a9991
junsangyooo <dbwnstkddb@gmail.com>
2023-01-08 17:10:04 -0500

    update
    update

---
+++ LeetCode75/First_Bad_Version/First_Bad_Version.cc
@@ -0,0 +1,19 @@
+// 278. First Bad Version C++
+
+// The API isBadVersion is defined for you.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1; 
+        int right = n;
+        while (left <= right) {
+            int mid = left + (right-left)/2;
+            if (isBadVersion(mid)) right = mid - 1;
+            else left = mid + 1;
+        }
+        if (isBadVersion(right)) return right;
+        else return right+1;
+    }
+};
---
+++ LeetCode75/First_Bad_Version/First_Bad_Version.py
@@ -0,0 +1,21 @@
+# 278. First Bad Version Python
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+def isBadVersion(version): bool
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left = 1
+        right = n
+        while left <= right:
+            mid = left + (right - left)/2
+            if isBadVersion(mid): right = mid - 1
+            else: left = mid + 1
+        if isBadVersion(right): return right
+        return right + 1
