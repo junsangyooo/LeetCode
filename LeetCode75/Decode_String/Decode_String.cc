98b8784c90dbec93040e17b148d7c6303c64af10
junsangyooo <dbwnstkddb@gmail.com>
2023-01-15 22:42:22 -0500

    update
    update

--- LeetCode75/Decode_String/Decode_String.cc
+++ LeetCode75/Decode_String/Decode_String.cc
@@ -1,2 +1,38 @@
 // 394. Decode String C++
+#include <string>
 
+class Solution {
+public:
+    int index = 0;
+    std::string decodeString(std::string s) {
+        int len = s.size();
+        std::string output = "";
+        int num = 0;
+        while (index < len) {
+            char c = s[index];
+            if (c >= '0' && c <= '9') {
+                num = num*10 + int(c-'0');
+            } else if (c == '[') {
+                output += decode(s, num);
+                num = 0;
+                continue;
+            } else if (c == ']') {
+                index++;
+                return output;
+            } else {
+                output += c;
+            }
+            index++;
+        }
+        return output;
+    }
+    std::string decode(std::string s, int num) {
+        index++;
+        std::string repeat = decodeString(s);
+        std::string output = "";
+        for (int i = 0; i < num; i++) {
+            output += repeat;
+        }
+        return output;
+    }
+};
