d50fa79a706e0b111ae75eba758cd990d38be7e2
junsangyooo <dbwnstkddb@gmail.com>
2023-01-24 14:35:00 -0500

    update
    update

---
+++ Algorithm_LevelOne/Populating_Next_Right_Pointers in_Each_Node/Populating_Next_Right_Pointers in_Each_Node.cc
@@ -0,0 +1,38 @@
+// 116. Populating Next Right Pointers in Each Node C++
+
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(nullptr), right(nullptr), next(nullptr) {}
+
+    Node(int _val) : val(_val), left(nullptr), right(nullptr), next(nullptr) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == nullptr) return root;
+        Node* leftmost = root;
+        while (leftmost->left) {
+            Node* head = leftmost;
+            while (head) {
+                head->left->next = head->right;
+                if (head->next) {
+                    head->right->next = head->next->left;
+                }
+                head = head->next;
+            }
+            leftmost = leftmost->left;
+        }
+        return root;
+    }
+};
---
+++ Algorithm_LevelOne/Populating_Next_Right_Pointers in_Each_Node/Populating_Next_Right_Pointers in_Each_Node.py
@@ -0,0 +1,28 @@
+# 116. Populating Next Right Pointers in Each Node Python
+
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        if not root:
+            return root
+        leftmost = root
+        while leftmost.left:
+            head = leftmost
+            while head:
+                head.left.next = head.right
+                if head.next:
+                    head.right.next = head.next.left
+                head = head.next
+            leftmost = leftmost.left
+        return root
