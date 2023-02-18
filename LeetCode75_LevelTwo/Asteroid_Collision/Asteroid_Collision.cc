87a790f5c7af3a1277583abf4ad894464d6ff741
junsangyooo <dbwnstkddb@gmail.com>
2023-02-18 01:03:36 -0500

    update
    update

--- LeetCode75_LevelTwo/Asteroid_Collision/Asteroid_Collision.cc
+++ LeetCode75_LevelTwo/Asteroid_Collision/Asteroid_Collision.cc
@@ -1,1 +1,27 @@
 // 735. Asteroid Collision C++
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> ans;
+        bool add = true;
+        for (auto &x: asteroids) {
+            while (!ans.empty() && x < 0 && ans.back() > 0) {
+                if ((x * -1) > ans.back()) {
+                    ans.pop_back();
+                    continue;
+                } else if ((x * -1) == ans.back()) {
+                    ans.pop_back();
+                    add = false;
+                }
+                add = false;
+                break;
+            }
+            if (add) ans.emplace_back(x);
+            add = true;
+        }
+        return ans;
+    }
+};
--- LeetCode75_LevelTwo/Asteroid_Collision/Asteroid_Collision.py
+++ LeetCode75_LevelTwo/Asteroid_Collision/Asteroid_Collision.py
@@ -1,1 +1,19 @@
 # 735. Asteroid Collision Python
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        for new in asteroids:
+            while ans and new < 0 < ans[-1]:
+                if ans[-1] < -new:
+                    ans.pop()
+                    continue
+                elif ans[-1] == -new:
+                    ans.pop()
+                break
+            else:
+                ans.append(new)
+        return ans
