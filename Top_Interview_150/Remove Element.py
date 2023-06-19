737d90f923228f9e2485f8894e8e554064ceb740
junsangyooo <dbwnstkddb@gmail.com>
2023-06-19 17:28:29 +0900

    update
    update

--- Top_Interview_150/Remove Element.py
+++ Top_Interview_150/Remove Element.py
@@ -1,1 +1,29 @@
 # 27. Remove Element
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        index1 = 0
+        index2 = len(nums) - 1
+        count = 0
+        while index1 <= index2:
+            if index1 == index2 and nums[index1] == val:
+                nums[index1] = None
+                break
+            if nums[index1] == val and nums[index2] == val:
+                nums[index2] = None
+                index2 -=1
+            elif nums[index1] == val:
+                nums[index1] = nums[index2]
+                nums[index2] = None
+                index1 += 1
+                index2 -= 1
+                count += 1
+            else:
+                count += 1
+                index1 += 1
+        return count
+    
