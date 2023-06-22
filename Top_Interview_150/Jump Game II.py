9e4ffbdff030088338d2bab7731e94d9115f0563
junsangyooo <dbwnstkddb@gmail.com>
2023-06-22 11:00:14 +0900

    update
    update

--- Top_Interview_150/Jump Game II.py
+++ Top_Interview_150/Jump Game II.py
@@ -1,1 +1,21 @@
 # 45. Jump Game II
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 1:
+            return 0
+        
+        jumps = 0
+        current_end = 0
+        furthest_reachable = 0
+
+        for i in range(len(nums) - 1):
+            furthest_reachable = max(furthest_reachable, i + nums[i])
+            if i == current_end:
+                jumps += 1
+                current_end = furthest_reachable
+        return jumps
+    
